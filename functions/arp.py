b33fe1281e346d5c79eb8d72665907d5bab2b080
Brikkel@Crush <rikvanbrakel24@gmail.com>
2023-06-07 14:14:26 +0200

    bug fix attempt mitm 5, removed debug

--- functions/arp.py
+++ functions/arp.py
@@ -55,12 +55,8 @@ def mitm_arp(victim_1_ip, victim_2_ip, interface):
         print(f'finding victim mac addresses on {interface}')
         victim_1_mac = get_target_mac(victim_1_ip, interface)
         victim_2_mac = get_target_mac(victim_2_ip, interface)
-        print(f'The mac address of the first victim: {victim_1_mac}')
-        print(f'The mac address of the second victim: {victim_2_mac}')
 
         while True:
-            print(f'The mac address of the first victim: {victim_1_mac}')
-            print(f'The mac address of the second victim: {victim_2_mac}')
             poison(victim_1_ip, victim_2_ip, victim_1_mac, interface)
             poison(victim_2_ip, victim_1_ip, victim_2_mac, interface)
             sleep(2)
