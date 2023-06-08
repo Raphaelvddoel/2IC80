44e71c5d3d5afbb02e29c2b9a6c1cf2203948244
Raphael van den Doel <doelraphael@gmail.com>
2023-06-08 11:57:44 +0200

    small improvements in setup

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-
 setup(
     name = 'CuckooByte',
     version = '0.0.1',
     author = '-',
     author_email = '-',
     license = 'MIT',
-    description = 'A commandline tool that provides simple pennetration testing capabilities.',
+    description = 'A command line tool that provides simple penetration testing capabilities.',
     long_description = long_description,
     long_description_content_type = "text/markdown",
     url = 'https://github.com/Raphaelvddoel/2IC80',
