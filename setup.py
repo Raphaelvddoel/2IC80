2889ec603ac2ee429bef76995443a08efb1c9586
Raphael van den Doel <doelraphael@gmail.com>
2023-06-11 14:14:27 +0200

    update requirement.txt

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open('requirements.txt', encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name = 'CuckooByte',
     version = '1.0.0',
@@ -16,6 +19,7 @@ setup(
     py_modules = ['cuckoo_byte', 'functions'],
     packages = find_packages(),
     python_requires='>=3.10',
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
